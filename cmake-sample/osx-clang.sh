1627066fd47d08e8f3f6a9058537089bd255ee7e
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2016-05-23 09:30:15 +0900

    Updated sample script file for using CMake

--- cmake-sample/osx-clang.sh
+++ cmake-sample/osx-clang.sh
@@ -1,13 +1,5 @@
-#ALPSCore
 export ALPSCore_DIR=/opt/ALPSCore
-
-#Path to the directory where header files of Eigen3 exit
-export EIGEN3_INCLUDE_DIR=/opt/local/include/eigen3
-
-#MPI compiler (non-MPI does not work)
-export CXX=mpicxx-openmpi-mp
-
-#Path to the root of the boost library files. We need only header files.
+export CXX=/opt/local/bin/mpicxx-openmpi-mp
 export BOOST_ROOT=~/work/src/boost_1_59_0
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/opt/cthyb path-to-impsolver-matrix-alpscore
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/opt/cthyb path_to_directory_containing_source_files
