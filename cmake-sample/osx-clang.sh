0893096605046d338ccc9759e08e7344d93f5d3f
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2016-11-11 09:16:16 +0900

    Find MPI in the exact same way as ALPSCore

--- cmake-sample/osx-clang.sh
+++
@@ -1,5 +0,0 @@
-cmake\
-    -DALPSCore_DIR=/path/to/ALPSCore/install/dir \
-    -DCMAKE_INSTALL_PREFIX=/path/to/install/dir \
-    -DCMAKE_CXX_COMPILER=/path/to/C++/compiler \
-    /path/to/CT-HYB/dir
---
+++ cmake/EnableMPI.cmake
@@ -0,0 +1,48 @@
+if (ALPSCore_HAS_MPI)
+    # try compiling sample mpi code
+    set(scratch_directory ${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY})
+    set(mpi_test_file ${scratch_directory}/cmake_mpi_test.cpp)
+    file(WRITE ${mpi_test_file}
+            "#include <mpi.h>\n"
+            "int main(int argc, char **argv) {\n"
+            "  MPI_Init(&argc, &argv);\n"
+            "  MPI_Finalize();\n"
+            "}\n")
+    try_compile(compiler_has_mpi ${scratch_directory} ${mpi_test_file})
+
+    if (compiler_has_mpi)
+        set(ALPS_HAVE_MPI TRUE)
+        message(STATUS "Your compiler supports MPI: "  ${CMAKE_CXX_COMPILER})
+    else()
+        # CXX Compiler doesn't support MPI - try to locate it
+        message(STATUS "Compiler does not support MPI. Trying to find MPI")
+
+        find_package(MPI)
+        if (${MPI_CXX_FOUND})
+            message(STATUS "MPI : Found compiler ${MPI_CXX_COMPILER}")
+            # check that the versions of compilers are the same
+            execute_process(COMMAND ${MPI_CXX_COMPILER}   "-dumpversion" OUTPUT_VARIABLE mpi_version OUTPUT_STRIP_TRAILING_WHITESPACE)
+            execute_process(COMMAND ${CMAKE_CXX_COMPILER} "-dumpversion" OUTPUT_VARIABLE cxx_version OUTPUT_STRIP_TRAILING_WHITESPACE)
+            if (NOT ${mpi_version} EQUAL ${cxx_version})
+                message(WARNING "MPI compiler doesn't match the C++ compiler.
+MPI compiler is: ${MPI_CXX_COMPILER}
+MPI compiler version is: ${mpi_version}
+C++ compiler is: ${CMAKE_CXX_COMPILER}
+C++ compiler version is: ${cxx_version}
+Depending on your platform this may lead to problems.")
+            endif()
+            set(ALPS_HAVE_MPI TRUE)
+            list(APPEND CMAKE_CXX_FLAGS ${MPI_CXX_COMPILE_FLAGS})
+            message(STATUS "MPI : Using ${CMAKE_CXX_COMPILER}")
+            if (MPI_CXX_COMPILE_FLAGS)
+                message(STATUS "MPI : with options ${MPI_CXX_COMPILE_FLAGS}")
+            endif()
+            target_include_directories(${PROJECT_NAME} PUBLIC ${MPI_CXX_INCLUDE_PATH} ${MPI_C_INCLUDE_PATH})
+            target_link_libraries(${PROJECT_NAME} PUBLIC ${MPI_CXX_LIBRARIES})
+        else()
+            message(WARNING "MPI not found.")
+        endif(${MPI_CXX_FOUND})
+    endif()
+else()
+    message(FATAL_ERROR "Error: ALPSCore must be built with MPI support!")
+endif()
