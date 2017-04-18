783f0ce9e2252d2078209f5ae35e2e701d197841
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2017-04-18 09:55:49 +0900

    Adopted for the lastest ALPSCore-extensions

--- src/orthogonal_basis/basis.cpp
+++ src/orthogonal_basis/basis.cpp
@@ -1,5 +1,5 @@
 #include "basis.hpp"
 #include "basis.ipp"
 
-template class IrBasis<alps::gf_extension::ir::fermionic_basis>;
-template class IrBasis<alps::gf_extension::ir::bosonic_basis>;
+template class IrBasis<alps::gf_extension::fermionic_ir_basis>;
+template class IrBasis<alps::gf_extension::bosonic_ir_basis>;
